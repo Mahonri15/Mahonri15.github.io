871bd70e8a025e7829989b2c1e7923d7132f563e
Mahonri15 <38745675+Mahonri15@users.noreply.github.com>
2018-06-15 12:18:41 +0800

    table

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,9 +1,24 @@
-var tempF = document.getElementById("highlow");
-var speed = document.getElementById("windSpeed");
-var result = windChill (tempF, speed);
+ let h = document.getElementById("highTemp").innerHTML;
+ let l = document.getElementById("lowTemp").innerHTML;
+ let avg = getAverage(h, l);
+ document.getElementById("highlow").innerHTML = avg / 2;
+ 
+ function getAverage(h, l) {
+  let low = parseInt(l);
+  let high = parseInt(h);
+ 	let average = low + high;
+  return average;
+ }
+
+
+
+let tempF = document.getElementById("highlow").innerHTML;
+let speed = document.getElementById("windSpeed").innerHTML;
+let result = windChill(tempF, speed);
 document.getElementById("windChillFactor").innerHTML = result;
-function windChill (tempF, speed) {
-    var f = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
-    return result;
+
+
+function windChill(tempF, speed) {
+  let f = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
+  return f.toFixed(2);
 }
-	   	   
