0ba280ee6ea221f5579c641b577e316250d94bcb
Mahonri15 <38745675+Mahonri15@users.noreply.github.com>
2018-06-29 14:33:46 +0800

    finals

--- assignments/lesson-10/scripts/franklintowndata.js
+++ assignments/lesson-10/scripts/franklintowndata.js
@@ -10,7 +10,6 @@ dataRequest.onload = function() {
     document.getElementById("cityMotto").innerHTML = townData.towns[0].motto;
     document.getElementById("cityYear").innerHTML = townData.towns[0].yearFounded;
     document.getElementById("cityPopulation").innerHTML = townData.towns[0].currentPopulation;
-    document.getElementById("cityRainfall").innerHTML = townData.towns[0].averageRainfall;   
-}
+    document.getElementById("cityRainfall").innerHTML = townData.towns[0].averageRainfall;
     
-    //document.getElementById("event1").innerHTML = townData.event[0].March 4: March to the Drum of Donuts;
+}
