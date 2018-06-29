3fe267f6053e6308d5897289c0b3470ec5539aaf
Mahonri15 <38745675+Mahonri15@users.noreply.github.com>
2018-06-29 14:07:30 +0800

    final

---
+++ assignments/lesson-10/scripts/franklintowndata.js
@@ -0,0 +1,15 @@
+let dataRequest = new XMLHttpRequest();
+let apiURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+dataRequest.open("GET", apiURL, true);
+dataRequest.send();
+
+dataRequest.onload = function() {
+    let townData = JSON.parse(dataRequest.responseText);
+    
+    document.getElementById("cityName").innerHTML = townData.towns[0].name;
+    document.getElementById("cityMotto").innerHTML = townData.towns[0].motto;
+    document.getElementById("cityYear").innerHTML = townData.towns[0].yearFounded;
+    document.getElementById("cityPopulation").innerHTML = townData.towns[0].currentPopulation;
+    document.getElementById("cityRainfall").innerHTML = townData.towns[0].averageRainfall;
+    
+    //document.getElementById("event1").innerHTML = townData.event[0].March 4: March to the Drum of Donuts;
---
+++ assignments/lesson-10/scripts/towndata.js
@@ -0,0 +1,26 @@
+let dataRequest = new XMLHttpRequest();
+let apiURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+dataRequest.open("GET", apiURL, true);
+dataRequest.send();
+
+dataRequest.onload = function() {
+    let townData = JSON.parse(dataRequest.responseText);
+    
+    document.getElementById("cityName").innerHTML = townData.towns[0].name;
+    document.getElementById("cityMotto").innerHTML = townData.towns[0].motto;
+    document.getElementById("cityYear").innerHTML = townData.towns[0].yearFounded;
+    document.getElementById("cityPopulation").innerHTML = townData.towns[0].currentPopulation;
+    document.getElementById("cityRainfall").innerHTML = townData.towns[0].averageRainfall;
+    
+    document.getElementById("cityName1").innerHTML = townData.towns[1].name;
+    document.getElementById("cityMotto1").innerHTML = townData.towns[1].motto;
+    document.getElementById("cityYear1").innerHTML = townData.towns[1].yearFounded;
+    document.getElementById("cityPopulation1").innerHTML = townData.towns[1].currentPopulation;
+    document.getElementById("cityRainfall1").innerHTML = townData.towns[1].averageRainfall;
+    
+    document.getElementById("cityName2").innerHTML = townData.towns[3].name;
+    document.getElementById("cityMotto2").innerHTML = townData.towns[3].motto;
+    document.getElementById("cityYear2").innerHTML = townData.towns[3].yearFounded;
+    document.getElementById("cityPopulation2").innerHTML = townData.towns[3].currentPopulation;
+    document.getElementById("cityRainfall2").innerHTML = townData.towns[3].averageRainfall;
+}
